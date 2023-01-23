072dd95e2d37acda16d3e199a7b42af47dd084c3
hyuk12 <hotman7025@naver.com>
2023-01-23 18:32:03 +0900

    연산자

--- src/main/java/com/lannstark/lec04/Lec04Main.java
+++ src/main/java/com/lannstark/lec04/Lec04Main.java
@@ -6,7 +6,8 @@ public class Lec04Main {
 
   public static void main(String[] args) {
     JavaMoney money1 = new JavaMoney(1_000L);
-    JavaMoney money2 = new JavaMoney(2_000L);
+    JavaMoney money2 = money1;
+    JavaMoney money3 = new JavaMoney(1_000L);
     System.out.println(money1.plus(money2));
   }
 
---
+++ src/main/java/com/lannstark/lec04/Lec04Main.kt
@@ -0,0 +1,18 @@
+package com.lannstark.lec04
+
+fun main() {
+
+    val money1 = JavaMoney(2_000L)
+    val money2 = JavaMoney(1_000L)
+
+    if (money1 > money2) {
+        println("Money1 이 Money2 보다 금액이 큽니다.")
+    }
+
+    val money3 = JavaMoney(1_000L)
+    val money4 = money3
+    val money5 = JavaMoney(1_000L)
+
+    println(money3 === money4)
+    println(money3 == money5)
+}
